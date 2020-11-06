d18de8f7858e400eb971bdf37346dbfee0049daf
shahraiz <shahraizkayani@live.com>
2020-11-06 16:22:57 +0500

    Add project files.

---
+++ Youtube Playlist App/Controllers/HomeController.cs
@@ -0,0 +1,94 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Youtube_Playlist_App.Controllers
+{
+
+        public class HomeController : Controller
+        {
+
+            public ActionResult Index()
+            {
+
+
+                return View();
+            }
+            [HttpGet]
+
+            public string GetTime(string url, int min = 0)
+            {
+                try
+                {
+                    TimeSpan timeSpan = PlaylistApp.Driver(url, min);
+
+                    return $"The Total time in  Playlist is {timeSpan.Days} Days, {timeSpan.Hours} Hours, {timeSpan.Minutes} Minutes and {timeSpan.Seconds} Seconds";
+                }
+                catch
+                {
+                    return "Please Read the Instructions and Enter Playlist Id in Given Format";
+                }
+
+
+
+            }
+
+        }
+
+        public class PlaylistApp
+        {
+            public static TimeSpan Driver(string url, int min = 0)
+            {
+                var html = new WebClient().DownloadString("https://www.youtube.com/playlist?list=" + url.Trim());
+
+                string value = GetBetween(html, @"window[""ytInitialData""] = ", @"window[""ytInitialPlayerResponse""]");
+
+                JObject parsed = JObject.Parse(value);
+
+                var list = new List<string>();
+                int max = (int)parsed.SelectToken("sidebar.playlistSidebarRenderer.items.[0].playlistSidebarPrimaryInfoRenderer.stats.[0].runs.[0].text");
+
+                for (int i = min; i < 99 && i < max; i++)
+                {
+                    var token = parsed.SelectToken("contents.twoColumnBrowseResultsRenderer.tabs.[0].tabRenderer.content.sectionListRenderer.contents.[0].itemSectionRenderer.contents[0].playlistVideoListRenderer.contents.[" + i + "].playlistVideoRenderer.lengthText.simpleText");
+                    list.Add(token.ToString());
+                }
+                return Time(list);
+
+
+            }
+
+            private static TimeSpan Time(List<string> time)
+            {
+                TimeSpan timeSpan = new TimeSpan(0);
+                var list = new List<TimeSpan>();
+                foreach (var item in time)
+                {
+                    string timer = item.IndexOf(":") == -1 ? $"00:00:{item}" : item.IndexOf(":") == item.LastIndexOf(":") ? $"00:{item}" : item;
+                    TimeSpan times = TimeSpan.Parse(timer);
+                    list.Add(times);
+                }
+                foreach (TimeSpan value in list)
+                {
+                    timeSpan = timeSpan.Add(value);
+                }
+                return timeSpan;
+            }
+
+            private static string GetBetween(string content, string startString, string endString)
+            {
+                if (content.Contains(startString) && content.Contains(endString))
+                {
+                    int Start = content.IndexOf(startString, 0) + startString.Length;
+                    int End = content.IndexOf(endString, Start);
+                    return content.Substring(Start, (End - 6) - Start);
+                }
+                else
+                    return string.Empty;
+            }
+        }
+    }
---
+++ Youtube Playlist App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Youtube_Playlist_App.MvcApplication" Language="C#" %>
